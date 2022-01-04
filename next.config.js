9b49ad41ced61d317b18e960e124c301354f8012
colbyfayock <fayock@gmail.com>
2022-01-03 21:31:28 -0500

    milestone

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: ['media.graphcms.com']
+  }
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,61 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router'
+import { FaShoppingCart } from 'react-icons/fa';
+
+import Container from '@components/Container';
+
+import styles from './Header.module.scss';
+
+const Header = () => {
+  const { locale: activeLocale, locales, asPath } = useRouter();
+  const availableLocales = locales.filter(locale => locale !== activeLocale);
+  return (
+    <header className={styles.header}>
+      <Container className={styles.headerContainer}>
+        <p className={styles.headerTitle}>
+          <Link href="/">
+            <a>Space Jelly</a>
+          </Link>
+        </p>
+        <ul className={styles.headerLinks}>
+          <li>
+            <Link href="/categories/apparel">
+              <a>Apparel</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/categories/accessories">
+              <a>Accessories</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/stores">
+              <a>Find a Store</a>
+            </Link>
+          </li>
+        </ul>
+        <p className={styles.headerCart}>
+          <button>
+            <FaShoppingCart />
+            <span>
+              $0.00
+            </span>
+          </button>
+        </p>
+        <ul className={styles.headerLocales}>
+          { availableLocales.map(locale => {
+            return (
+              <li key={locale}>
+                <Link href={asPath} locale={locale}>
+                  <a>{ locale }</a>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </Container>
+    </header>
+  )
+}
+
+export default Header;
